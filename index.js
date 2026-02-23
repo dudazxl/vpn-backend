24b804b52534c48cb812a33a117e90d87a9d005f
dudazxl <eduardazx5@gmail.com>
2026-02-23 11:36:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/teste", (req, res) => {
   res.send("backend funcionando");
-});
-
-app.get("/gerar", (req, res) => {
-  const numero = Math.floor(Math.random() * 1000);
-  res.json({ usuario: "user" + numero });
 });
 
 const PORT = process.env.PORT || 3000;
